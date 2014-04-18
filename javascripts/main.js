df47d33828fa8dae30101129d4e394e72256005c
Wynout van der Veer <wynout@gmail.com>
2014-04-18 13:48:19 +0200

    Remove console.log

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,13 +5,7 @@
 	$('#button-wrapper button').on('click', function (event) {
 
 		var $this = $(this);
-		console.log(this);
-
 		$images.removeClass();
 		$images.addClass($this.attr('class'));
-
-
-
-
 	});
 })();
